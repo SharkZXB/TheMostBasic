f930e43c1084214c23cc04a98f1b675a987525a8
Shark <229153959@qq.com>
2020-03-30 22:10:48 +0800

    create project

---
+++ app/src/main/java/com/sharkz/themostbasic/MainActivity.java
@@ -0,0 +1,23 @@
+package com.sharkz.themostbasic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * ================================================
+ * 作    者：SharkZ
+ * 邮    箱：229153959@qq.com
+ * 创建日期：2020/3/30  21:35
+ * 描    述
+ * 修订历史：
+ * ================================================
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
